916b6b0b6800efe57a2ab02c2d9df81f73ca0793
dawid <dawidp789@gmail.com>
2022-04-11 19:25:51 +0200

    add RoadDifficultyServiceTest

---
+++ src/test/java/com/depe/roaddifficulties/RoadDifficultyServiceTest.java
@@ -0,0 +1,133 @@
+package com.depe.roaddifficulties;
+
+import com.depe.roaddifficulties.exceptions.WrongParamException;
+import com.depe.roaddifficulties.model.GddkiaApiClient;
+import com.depe.roaddifficulties.model.TrafficDifficulty;
+import com.depe.roaddifficulties.service.RoadDifficultiesService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.MockitoAnnotations;
+
+import java.io.*;
+import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class RoadDifficultyServiceTest {
+
+    private GddkiaApiClient client;
+    private RoadDifficultiesService underTest;
+    private String testText;
+
+    @BeforeEach
+    void setUp() {
+        File file = new File("test.txt");
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            StringBuilder builder = new StringBuilder();
+            br.lines().forEach(builder::append);
+            testText = builder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        client = mock(GddkiaApiClient.class);
+        MockitoAnnotations.openMocks(this);
+        underTest = new RoadDifficultiesService(client);
+    }
+
+
+    private static Stream<Arguments> provideVoivodeships() {
+        return Stream.of(
+                Arguments.of("lubelskie", "lubelskie"),
+                Arguments.of("lodzkie", "łódzkie"),
+                Arguments.of("mazowieckie", "mazowieckie"),
+                Arguments.of("malopolskie", "małopolskie"),
+                Arguments.of("podkarpackie", "podkarpackie"),
+                Arguments.of("slaskie", "śląskie"),
+                Arguments.of("wielkopolskie", "wielkopolskie"),
+                Arguments.of("warminsko_mazurskie", "warmińsko-mazurskie"),
+                Arguments.of("kujawsko_pomorskie", "kujawsko-pomorskie"),
+                Arguments.of("lubuskie", "lubuskie"),
+                Arguments.of("swietokrzyskie", "świętokrzyskie"),
+                Arguments.of("opolskie", "opolskie"),
+                Arguments.of("pomorskie", "pomorskie"),
+                Arguments.of("zachodniopomorskie", "zachodniopomorskie"),
+                Arguments.of("podlaskie", "podlaskie"),
+                Arguments.of("dolnoslaskie", "dolnośląskie")
+        );
+    }
+
+    @ParameterizedTest(name = "{0} should be {1}")
+    @MethodSource("provideVoivodeships")
+    void shouldReturnResultsWithVoivodeship(String voivodeship, String expected) {
+        //when
+        when(client.getResponse()).thenReturn(testText);
+        var result = underTest.getResultsByVoivodeship(voivodeship);
+        Set<String> results = result.getTrafficDifficulties().stream().map(TrafficDifficulty::getVoivodeship)
+                .collect(Collectors.toSet());
+        //then
+        assertThat(results).hasSize(1).containsExactly(expected);
+    }
+
+    private static Stream<Arguments> provideWrongVoivodeships() {
+        return Stream.of(
+                Arguments.of("lubelkie"),
+                Arguments.of("lo"),
+                Arguments.of("lubelsk")
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("provideWrongVoivodeships")
+    void shouldThrow(String voivodeship) {
+        var result = assertThrows(WrongParamException.class, () -> underTest.getResultsByVoivodeship(voivodeship));
+        assertThat(result.getMessage()).isEqualTo(voivodeship + " is not a voivodeship");
+    }
+
+    private static Stream<Arguments> provideRoads() {
+        return Stream.of(
+                Arguments.of("a2"),
+                Arguments.of("a1")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideRoads")
+    void shouldReturnResultsWithRoad(String road) {
+        //when
+        when(client.getResponse()).thenReturn(testText);
+        var result = underTest.getResultsByRoad(road);
+        Set<String> results = result.getTrafficDifficulties().stream().map(TrafficDifficulty::getRoad).collect(Collectors.toSet());
+        //then
+        assertThat(results).hasSize(1).containsExactly(road.toUpperCase(Locale.ROOT));
+    }
+
+    private static Stream<Arguments> provideDistance() {
+        return Stream.of(
+                Arguments.of(52.769575, 18.848718, 300.0, 231),
+                Arguments.of(52.769575, 18.848718, 200.0, 91),
+                Arguments.of(52.769575, 18.848718, 100.0, 19),
+                Arguments.of(52.769575, 18.848718, 50.0, 4),
+                Arguments.of(52.769575, 18.848718, 25.0, 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDistance")
+    void getResultsByDistance(Double latitude, Double longitude, Double distance, int size) {
+        //when
+        when(client.getResponse()).thenReturn(testText);
+        var result = underTest.getResultsByDistance(latitude, longitude, distance);
+        var results = result.getTrafficDifficulties();
+        //then
+        assertThat(results).hasSize(size);
+    }
+}
