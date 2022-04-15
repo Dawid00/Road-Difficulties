b1c16edaa575a25d74e041e6e33d330687df0faa
dawid <dawidp789@gmail.com>
2022-04-15 15:17:36 +0200

    rename from handler controller to TrafficDifficultiesExceptionHandler

--- src/main/java/com/depe/roaddifficulties/controller/handler/TrafficDifficultiesExceptionHandler.java
+++ src/main/java/com/depe/roaddifficulties/controller/handler/TrafficDifficultiesExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class HandlerController {
+public class TrafficDifficultiesExceptionHandler {
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(WrongParamException.class)
