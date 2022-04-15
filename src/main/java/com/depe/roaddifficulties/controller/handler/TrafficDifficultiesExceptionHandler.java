9618c343f3d94c4aa812d46cbef15f017a4b95e0
dawid <dawidp789@gmail.com>
2022-04-15 23:34:55 +0200

    change return value of methods in TrafficDifficultiesExceptionHandler

--- src/main/java/com/depe/roaddifficulties/controller/handler/TrafficDifficultiesExceptionHandler.java
+++ src/main/java/com/depe/roaddifficulties/controller/handler/TrafficDifficultiesExceptionHandler.java
@@ -2,25 +2,22 @@ package com.depe.roaddifficulties.controller.handler;
 
 import com.depe.roaddifficulties.exceptions.InternalServerException;
 import com.depe.roaddifficulties.exceptions.WrongParamException;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class TrafficDifficultiesExceptionHandler {
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+
+
     @ExceptionHandler(WrongParamException.class)
-    public String handleWrongParamException(WrongParamException exception){
-        return exception.getMessage();
+    public ResponseEntity<String> handleWrongParamException(WrongParamException exception){
+        return ResponseEntity.badRequest().body(exception.getMessage());
     }
-    @ResponseBody
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+
     @ExceptionHandler(InternalServerException.class)
-    public String handleInternalServerException(InternalServerException exception){
-        return exception.getMessage();
+    public ResponseEntity<String> handleInternalServerException(InternalServerException exception){
+        return ResponseEntity.internalServerError().body(exception.getMessage());
     }
 
 }
